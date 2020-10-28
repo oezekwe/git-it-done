9ba420c69fe726374773d7fec2bbe61d1cc08ae1
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-27 20:39:34 -0500

    search topics

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl= document.querySelector("#user-form");
 var nameInputEl= document.querySelector("#username");
 var repoContainerEl= document.querySelector("#repos-container");
 var repoSearchTerm= document.querySelector("#repo-search-term");
+var languageButtonsEl= document.querySelector("#language-buttons");
 
 var formSubmitHandler= function(event){
     event.preventDefault();
@@ -17,6 +18,21 @@ var formSubmitHandler= function(event){
         alert("Please enter a GitHub username");
     }
     console.log(event);
+};
+
+var getFeaturedRepos= function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        }
+        else{
+            alert("Error: "+ response.statusText);
+        }
+    });
 };
 
 var displayRepos= function(repos, searchTerm){
@@ -85,5 +101,16 @@ var getUserRepos= function(user){
         alert("Unable to connect to GitHub");
     });
 };
+
+function buttonClickHandler(event){
+    var language= event.target.getAttribute("data-language");
+    console.log(language);
+
+    if(language){
+        getFeaturedRepos(language);
+        repoContainerEl.textContent= "";
+    }
+}
   
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
